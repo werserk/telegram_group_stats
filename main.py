da00c84d494c285997b27e3d3096546526a1dff0
werserk <werserk@inbox.ru>
2024-12-03 04:22:53 +0300

    new service for groups: get group id by username + get participants of the group by id

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+# main.py
+
 import os
 
 from dotenv import load_dotenv
 
 from app.service.client import TDLibClient
+from app.service.services.chat_member_service import ChatMemberService
 
 load_dotenv()
 
 
 def main():
-    tdlib_client = TDLibClient(api_id=os.getenv("API_ID"), api_hash=os.getenv("API_HASH"))
+    api_id = os.getenv("API_ID")
+    api_hash = os.getenv("API_HASH")
+
+    tdlib_client = TDLibClient(api_id=api_id, api_hash=api_hash)
+    chat_member_service = ChatMemberService(tdlib_client)
+
+    # chat_tag = input("Enter the chat username: ")
+    # chat_id = chat_member_service.get_chat_id_by_username(chat_tag)
+    # print(f"Chat ID: {chat_id}")
+
+    chat_id_input = input("Enter the chat ID: ")
+    try:
+        chat_id = int(chat_id_input)
+    except ValueError:
+        print("Invalid chat ID. Please enter a numeric chat ID.")
+        return
+
+    members = chat_member_service.get_chat_members(chat_id)
+
+    print(f"Total members in chat {chat_id}: {len(members)}")
+    for member in members:
+        user_id = member["user_id"]
+        print(f"User ID: {user_id}")
 
 
 if __name__ == "__main__":
